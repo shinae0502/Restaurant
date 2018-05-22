92238bcfc5dea92e445330ceed13bbca9a157d5f
sryang <sarang628@naver.com>
2018-05-23 04:12:56 +0900

    맛집찾기

--- app/src/main/java/com/study/restaurant/fragment/FindRestaurantFragment.java
+++ app/src/main/java/com/study/restaurant/fragment/FindRestaurantFragment.java
@@ -1,8 +1,14 @@
 package com.study.restaurant.fragment;
 
+import android.graphics.Paint;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -10,11 +16,16 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.study.restaurant.FunctionImpl;
 import com.study.restaurant.R;
+import com.viewpagerindicator.CirclePageIndicator;
 
 public class FindRestaurantFragment extends Fragment implements FunctionImpl.FindRestaurant {
+
+    private RecyclerView findRestaurantRv;
+    private ViewPager bannerPager;
 
     public FindRestaurantFragment() {
         // Required empty public constructor
@@ -36,7 +47,49 @@ public class FindRestaurantFragment extends Fragment implements FunctionImpl.Fin
         setHasOptionsMenu(true);
         View v = inflater.inflate(R.layout.fragment_find_restaurant, container, false);
         ((AppCompatActivity) getActivity()).setSupportActionBar((Toolbar) v.findViewById(R.id.toolbar));
+
+        findRestaurantRv = v.findViewById(R.id.findRestaurantRv);
+        findRestaurantRv.setLayoutManager(new LinearLayoutManager(getContext()));
+        findRestaurantRv.setAdapter(new RvAdt());
+        findRestaurantRv.setNestedScrollingEnabled(false);
+
+        bannerPager = v.findViewById(R.id.bannerPager);
+        bannerPager.setAdapter(new ViewPagerAdapter(getChildFragmentManager()));
+
+        CirclePageIndicator circlePageIndicator = v.findViewById(R.id.indicator);
+        circlePageIndicator.setViewPager(bannerPager);
+
+        TextView textView = v.findViewById(R.id.location);
+        textView.setPaintFlags(textView.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
+
         return v;
+    }
+
+    public class RvAdt extends RecyclerView.Adapter<RvHolder> {
+
+        @Override
+        public RvHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.item, parent, false);
+            RvHolder rvHolder = new RvHolder(v);
+            return rvHolder;
+        }
+
+        @Override
+        public void onBindViewHolder(RvHolder holder, int position) {
+
+        }
+
+        @Override
+        public int getItemCount() {
+            return 100;
+        }
+    }
+
+    public class RvHolder extends RecyclerView.ViewHolder {
+
+        public RvHolder(View itemView) {
+            super(itemView);
+        }
     }
 
     @Override
@@ -142,5 +195,43 @@ public class FindRestaurantFragment extends Fragment implements FunctionImpl.Fin
     @Override
     public void setShowTopButton(boolean show) {
         //TODO::
+    }
+
+    private class ViewPagerAdapter extends FragmentStatePagerAdapter {
+
+        public ViewPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return BlankFragment.newInstance();
+        }
+
+        @Override
+        public int getCount() {
+            return 10;
+        }
+    }
+
+    public static class BlankFragment extends Fragment {
+        public BlankFragment() {
+        }
+
+        public static BlankFragment newInstance() {
+            BlankFragment fragment = new BlankFragment();
+            return fragment;
+        }
+
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            return inflater.inflate(R.layout.fragment_blank, container, false);
+        }
     }
 }
