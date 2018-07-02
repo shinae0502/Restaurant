752c329709599817042e6fc3c9c479dcdda7dcd6
sryang <sarang628@naver.com>
2018-07-02 15:04:18 +0700

    맛집 상세

--- app/src/main/java/com/study/restaurant/activity/RestaurantDetailActivity.java
+++ app/src/main/java/com/study/restaurant/activity/RestaurantDetailActivity.java
@@ -1,8 +1,12 @@
 package com.study.restaurant.activity;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.support.design.widget.AppBarLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
 
 import com.study.restaurant.R;
 import com.study.restaurant.model.Store;
@@ -10,16 +14,64 @@ import com.study.restaurant.model.Store;
 public class RestaurantDetailActivity extends AppCompatActivity {
 
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restaurant_detail);
+
+        AppBarLayout appBarLayout = findViewById(R.id.app_bar);
+        final Toolbar actionBar = findViewById(R.id.actionBar);
+        appBarLayout.addOnOffsetChangedListener(new AppBarStateChangeListener() {
+            @Override
+            public void onStateChanged(AppBarLayout appBarLayout, State state) {
+                if (state == State.IDLE) {
+                    actionBar.setSelected(false);
+                    actionBar.setBackgroundColor(Color.WHITE);
+//                    nestedScrollView.stopNestedScroll();
+                } else if (state == State.COLLAPSED) {
+                    actionBar.setSelected(true);
+                    actionBar.setBackgroundColor(getResources().getColor(R.color.orange));
+                }
+            }
+        });
     }
 
     public static void go(AppCompatActivity appCompatActivity, Store store) {
         Intent intent = new Intent(appCompatActivity, RestaurantDetailActivity.class);
         intent.putExtra("store", store);
         appCompatActivity.startActivity(intent);
+    }
+
+    public abstract static class AppBarStateChangeListener implements AppBarLayout.OnOffsetChangedListener {
+
+        public enum State {
+            EXPANDED,
+            COLLAPSED,
+            IDLE
+        }
+
+        private State mCurrentState = State.IDLE;
+
+        @Override
+        public final void onOffsetChanged(AppBarLayout appBarLayout, int i) {
+            if (i == 0) {
+                if (mCurrentState != State.EXPANDED) {
+                    onStateChanged(appBarLayout, State.EXPANDED);
+                }
+                mCurrentState = State.EXPANDED;
+            } else if (Math.abs(i) >= appBarLayout.getTotalScrollRange()) {
+                if (mCurrentState != State.COLLAPSED) {
+                    onStateChanged(appBarLayout, State.COLLAPSED);
+                }
+                mCurrentState = State.COLLAPSED;
+            } else {
+                if (mCurrentState != State.IDLE) {
+                    onStateChanged(appBarLayout, State.IDLE);
+                }
+                mCurrentState = State.IDLE;
+            }
+        }
+
+        public abstract void onStateChanged(AppBarLayout appBarLayout, State state);
     }
 }
