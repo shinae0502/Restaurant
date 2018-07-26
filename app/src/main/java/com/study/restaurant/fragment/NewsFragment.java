bd457cc636caa02b97c54025aad92c69350f4de2
yangsarang <sarang628@naver.com>
2018-07-26 23:06:24 +0900

    소식

--- app/src/main/java/com/study/restaurant/fragment/NewsFragment.java
+++ app/src/main/java/com/study/restaurant/fragment/NewsFragment.java
@@ -2,6 +2,8 @@ package com.study.restaurant.fragment;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -28,7 +30,38 @@ public class NewsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View v = inflater.inflate(R.layout.fragment_news, container, false);
+        View v = inflater.inflate(R.layout.fragment_news_list, container, false);
+        RecyclerView recyclerView = v.findViewById(R.id.recyclerView);
+        recyclerView.setAdapter(new NewsRvAdt());
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
         return v;
     }
+
+    public class NewsRvAdt extends RecyclerView.Adapter<RvHolder> {
+
+        @Override
+        public RvHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_news, parent, false);
+            RvHolder rvHolder = new RvHolder(v);
+            return rvHolder;
+        }
+
+        @Override
+        public void onBindViewHolder(RvHolder holder, int position) {
+
+        }
+
+        @Override
+        public int getItemCount() {
+            return 100;
+        }
+    }
+
+    public class RvHolder extends RecyclerView.ViewHolder {
+
+        public RvHolder(View itemView) {
+            super(itemView);
+        }
+    }
+
 }
