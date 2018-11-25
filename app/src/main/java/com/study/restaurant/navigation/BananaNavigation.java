ba78df614aaec8e50cdbf404ca3f36ab6f41df94
sarangyang <sarang628@naver.com>
2018-11-26 01:28:58 +0800

    체크인 추가

---
+++ app/src/main/java/com/study/restaurant/navigation/BananaNavigation.java
@@ -0,0 +1,52 @@
+package com.study.restaurant.navigation;
+
+import com.study.restaurant.model.Store;
+
+public class BananaNavigation {
+    public interface SplashNavigation {
+        void goLogin();
+
+        void goMain();
+    }
+
+    public interface LoginNavigation {
+        void goMain();
+    }
+
+    public interface MainNavigation {
+        void goFindRestaurant();
+
+        void goMangoPick();
+
+        void goRegister();
+
+        void goNews();
+
+        void goMyInformation();
+
+        void registerShowAnimation();
+
+        void hideMenu();
+
+        void rotationMenu(boolean b);
+    }
+
+    public interface FindRestaurantNavigation {
+
+        void showSelectRegionPopup();
+
+        void showSortPopup();
+
+        void showBoundaryPopup();
+
+        void showFilterPopup();
+
+        void goSearch();
+
+        void goDeatilRestaurant(Store store);
+
+        void rvToTop();
+
+        void showErrorPopup(String msg);
+    }
+}
